c8d4003aebd6ecfbd4ce623b0e39b0def286f102
sirilijenab <46484609+sirilijenab@users.noreply.github.com>
2019-01-09 14:42:32 -0800

    Add files via upload
    # Just For Fun Project
    # :)
    # ilijenab

---
+++ Love.py
@@ -0,0 +1,15 @@
+print('''
+Alan Bayad Begi Cheghadr Mohamad REza
+roo Vaghean Dost Dari !?
+Bayad Ba Addad Begi Cheghad Dosesh DAri
+Zir 500 ta bawshe ghabol nmishi ! :)
+''')
+a = int(input('In type kon cheghadr dosesh dari ?!'))
+if a > 500:
+    print('Vay Bavaram nmishe Akhe manm ', a,'doset daram')
+elif a <= 0:
+        print('Kheyli Bishooori :|')
+elif a < 500:
+        print('Kheyli Gavi ')
+else:
+    print('Azizam Adad ro Dorost Vared kon :|')
